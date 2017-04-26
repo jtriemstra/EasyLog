497e42eb30d6f4380090bdcd0b99f8beef0cb28e
Joel Triemstra <JTriemstra@jtriemstraw7.VML.LAN>
2017-04-26 08:28:42 -0400

    starting .NET test app

---
+++ EasyButtonDotNet/TestScriptRunner/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestScriptRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const String PATH_TO_GIT_SHELL = @"C:\DevTools\Git\bin\sh.exe";
+            const String PATH_TO_SHELL_SCRIPT = @"C:\TestProjects\EasyButton\EasyButtonDotNet\TestScriptRunner\test.sh";
+        }
+    }
+}
